40c9a351b47bae641de41f246e9dbf7aadf2811f
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-06-23 13:51:48 +0530

    user active packages

--- routes/web.php
+++ routes/web.php
@@ -29,21 +29,21 @@ Route::get('/', function () {
 Route::get('/redirects', [HomeController::class,'index'])->middleware(['auth','verified']);
 Route::get('/dashboard', [HomeController::class,'index'])->middleware(['auth','verified']);
 
-Route::resource('package', PackageController::class);
-Route::resource('buy_package', BuypackageController::class);
-Route::get('buy_package/{id}/progress',[BuypackageController::class,'buy']);
+Route::resource('package', PackageController::class)->middleware(['auth','verified']);
+Route::resource('buy_package', BuypackageController::class)->middleware(['auth','verified']);
+Route::get('buy_package/{id}/progress',[BuypackageController::class,'buy'])->middleware(['auth','verified']);
 Route::get('active_packages', [BuypackageController::class,'active']);
-Route::get('/user/profile', [UserController::class,'user']);
+Route::get('/user/profile', [UserController::class,'user'])->middleware(['auth','verified']);
 
-Route::resource('user_buy_package', UserbuypackageController::class);
+Route::resource('user_buy_package', UserbuypackageController::class)->middleware(['auth','verified']);
 
 
-Route::resource('kyc', KycController::class);
+Route::resource('kyc', KycController::class)->middleware(['auth','verified']);
 
-Route::get('/user/profile', [UserController::class,'user']);
-Route::resource('user', UserController::class);
+Route::get('/user/profile', [UserController::class,'user'])->middleware(['auth','verified']);
+Route::resource('user', UserController::class)->middleware(['auth','verified']);
 
-Route::resource('wallet', WalletController::class);
+Route::resource('wallet', WalletController::class)->middleware(['auth','verified']);
 
 Route::middleware([
     'auth:sanctum',
