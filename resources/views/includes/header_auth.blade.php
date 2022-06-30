65e787efd3261826f116e3f1fddb83232ed6ae4b
unknown <lucianmacwolf@gmail.com>
2022-06-30 19:32:00 +0530

    ui

--- resources/views/includes/header_auth.blade.php
+++ resources/views/includes/header_auth.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Zenix -  Crypto Admin Dashboard </title>
+    <title>{{ config('app.name', 'Laravel') }} </title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon.png') }}">
 	<link href="{{ asset('vendor/bootstrap-select/dist/css/bootstrap-select.min.css') }}" rel="stylesheet">
