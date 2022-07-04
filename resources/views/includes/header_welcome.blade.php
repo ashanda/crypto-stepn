5e697cf59f23ba3f87dc0d072a30d47a79c38ad4
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-07-04 10:58:50 +0530

    test navbar

--- resources/views/includes/header_welcome.blade.php
+++ resources/views/includes/header_welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,40 +8,55 @@
     <title>LEMACO</title>
 
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body class="body">
 
 
-	<!---header-->
+    <!---header-->
     <div class="header">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
-                         <div class="center-desk">
-                            <div class="logo">
-                               <a href="https://lemaconet.com" ><img src="assets/images/lemaconet F.png"> </a>
+                    <div class="center-desk">
+                        <div class="logo">
+                            <a href="https://lemaconet.com"><img src="assets/images/lemaconet F.png"> </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div class="navbar-nav">
+                                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                                <a class="nav-item nav-link" href="#">Login</a>
+                                <a class="nav-item nav-link" href="#"><button class="nav_btn">Get Started</button></a>
                             </div>
-                         </div>
-                   </div>
-                   <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-                      <div class="menu-area topnav" id="myTopnav">
-                            <nav class="main-menu">
-                               <ul class="menu-area-main">
+                        </div>
+                    </nav>
+
+
+                    <!-- <div class="menu-area topnav" id="myTopnav">
+                        <nav class="main-menu">
+                            <ul class="menu-area-main">
                                 <div id="selectField">
-                                        <a href="https://lemaconet.com">Home</a>
-                                        <!---a href="#">About </a>
-                                        <a href="#">Contact Us</a---->
-                                        <a href="/login">Login</a>
-                                        <a href="/register"><button class="nav_btn">Get Started</button></a>
+                                    <a href="https://lemaconet.com">Home</a>
+                                    
+                                    <a href="/login">Login</a>
+                                    <a href="/register"><button class="nav_btn">Get Started</button></a>
 
                                 </div>
-                               </ul>
-                            </nav>
-                      </div>
+                            </ul>
+                        </nav> -->
+                    </div>
                 </div>
             </div>
         </div>
