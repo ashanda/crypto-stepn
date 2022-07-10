634b853034eac98065397cce9edf41b5d1ebe559
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-07-10 21:01:34 +0530

    binary fix

--- resources/views/admin/package_earn/index.blade.php
+++ resources/views/admin/package_earn/index.blade.php
@@ -8,12 +8,13 @@
          <div class="container mt-2">
             <div class="container mt-4">
                 
-
+                <!--
                   <form name="add-blog-post-form" id="add-blog-post-form" method="post" action="{{url('package_earn_tranfer')}}">
                     @csrf
                      
                      <button type="submit" class="btn btn-primary">Tranfer Package Commission</button>
                    </form>
+                  -->
 
 
         </div>
