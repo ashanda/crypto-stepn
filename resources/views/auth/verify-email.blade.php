8dd6fb8d89d7393638b1708953e5fc7669b09102
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-07-29 11:09:44 +0530

    verification email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -15,7 +15,7 @@
                     @endif
 
                     {{ __('Before proceeding, confirm your email address.') }}
-                    {{ __('If you did not receive the email') }},
+                    ,
                     <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
                         @csrf
                         <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to send email verification link') }}</button>.
