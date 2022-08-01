cf5025439461e55ffc3e100a2ba5d4a2effd1086
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-08-01 17:09:13 +0530

    Comment notification

--- app/Listeners/SendNewUserNotification.php
+++ app/Listeners/SendNewUserNotification.php
@@ -10,11 +10,6 @@ class SendNewUserNotification
 {
     public function handle($event)
     {
-       /* $admins = User::whereHas('roles', function ($query) {
-            $query->where('id', 1);
-        })->get();
-
-    Notification::send($admins, new NewUserNotification($event->user));
-   *\
- }
+      
+    }
 }
