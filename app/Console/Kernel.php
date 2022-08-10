041be9876e58a40b0a90dd0c5b56e569e762abee
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-08-10 15:53:05 +0530

    new modification of package commission

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('PackageEarn:cron')->dailyAt('00:00');
-        $schedule->command('backup:run')->everyMinute();
+        $schedule->command('backup:run')->dailyAt('00:00');
     }
 
     /**
