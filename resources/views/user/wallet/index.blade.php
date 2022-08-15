930d8458f20a6ae46bd58a40ee61bb7daadaf820
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-08-15 11:36:16 +0530

    report

--- resources/views/user/wallet/index.blade.php
+++ resources/views/user/wallet/index.blade.php
@@ -157,7 +157,7 @@
 												@else
 												@foreach ($data as $package)
 												<tr>
-													<td>${{ $package->amount+$package->fee }}</td>												
+													<td>${{ $package->amount}}</td>												
 													<td>{{ $package->p2p_id  }} </td>
 													<td>{{ $package->currency_type  }} </td>
 													<td>{{ $package->network  }} </td>
