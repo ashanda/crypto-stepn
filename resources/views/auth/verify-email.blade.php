adc0c6ed2c1f9a552b24c9783b442033839fbf26
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-08-23 11:32:25 +0530

    email alert test

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -9,20 +9,30 @@
 
                 <div class="card-body">
                     @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ __('A fresh verification link has been sent to your email address.') }}
+                    </div>
                     @endif
 
                     {{ __('Before proceeding, confirm your email address.') }}
                     ,
                     <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to send email verification link') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to resend email verification link') }}</button>.
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    function onload_alert() {
+
+        alert(" Verification email has been sent. Please check your emails");
+
+    }
+
+    window.onload = onload_alert();
+</script>
 @endsection
