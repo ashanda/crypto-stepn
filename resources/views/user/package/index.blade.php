85017cf038f674b1c0af0794e33bdad402dfc25e
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-08-23 12:09:41 +0530

    make package section 4 colomn

--- resources/views/user/package/index.blade.php
+++ resources/views/user/package/index.blade.php
@@ -10,7 +10,7 @@
 <style>
     /* package-section Styles */
     .package-section {
-        padding-left: 25% !important;
+        padding-left: 360px !important;
     }
 
 
@@ -47,7 +47,7 @@
    
     <div class="row">
     @foreach ( $data as $package)
-    <div class="col-xl-4 col-lg-4 col-sm-4">
+    <div class="col-xl-3 col-lg-3 col-sm-4">
         <div class="card">
             <div class="card-body">
 
